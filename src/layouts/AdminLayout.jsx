d0666b4ceea25723af6c1226a36e7c2de5c1328a
Usman <usmanmalik9680@gmail.com>
2025-12-16 21:58:11 +0500

    Adjust sidebar of admin layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,3 +1,4 @@
+import UserSideBar from "../components/common/UserSideBar";
 import Navbar from "../components/layout/Navbar";
 import { Container, Box } from "@mui/material";
 
@@ -6,14 +7,10 @@ export default function AdminLayout({ children }) {
     <>
       <Navbar />
       <Box sx={{ display: "flex" }}>
-        <Box sx={{ width: 250, bgcolor: "#f1f5f9", height: "100vh", p: 2 }}>
-          <h3>Admin Panel</h3>
-          <ul>
-            <li>Dashboard</li>
-            <li>Users</li>
-            <li>Listings</li>
-            <li>Complaints</li>
-          </ul>
+        <Box sx={{ width: 250, bgcolor: "#f1f5f9", height: "100vh", mb: 4 }}>
+          <Box>
+            <UserSideBar/>
+          </Box>
         </Box>
 
         <Container sx={{ mt: 4 }}>{children}</Container>
